a1b4e418d6fbd34739be0f247755eba85594eaa7
PAUL_W_SALTZMAN <paul.saltzman@gmail.com>
2025-09-21 14:01:31 -0500

    adding darkmode / lightmode

---
+++ DevPortfolio/wwwroot/theme-toggle.js
@@ -0,0 +1,8 @@
+﻿const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+    const currentTheme = body.getAttribute("data-bs-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    body.setAttribute("data-bs-theme", newTheme);
+});
