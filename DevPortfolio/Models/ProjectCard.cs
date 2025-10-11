d8b047c51e82e7dfc67a0e6fd6a5dc6a82a24011
PAUL_W_SALTZMAN <paul.saltzman@gmail.com>
2025-10-11 15:44:53 -0500

    save point

---
+++ DevPortfolio/Models/ProjectCard.cs
@@ -0,0 +1,15 @@
+﻿namespace DevPortfolio.Models
+{
+
+    public class ProjectCard
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string ImageUrl { get; set; } = "";
+        public string PrimaryColor { get; set; } = "blue";
+        public string SecondaryColor { get; set; } = "purple";
+        public bool IsActive { get; set; } = false;
+    }
+
+
+}
